14d4659fdbe5f5d8a4d71f4d1e10b6566eb44560
przem14 <przem.wawrzyniak@gmail.com>
2016-05-04 21:18:22 +0200

    Embedded LanguatorViewController in navigation bar

--- Languator/LanguatorViewController.swift
+++ Languator/LanguatorViewController.swift
@@ -16,12 +16,10 @@ class LanguatorViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
