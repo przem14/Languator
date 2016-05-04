8b6e5b55c9e02d02e79bcfedf9988312f1bbfd50
przem14 <przem.wawrzyniak@gmail.com>
2016-05-04 20:43:44 +0200

    Added main table view controller

--- Languator/LanguatorViewController.swift
+++ Languator/LanguatorViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LanguatorViewController.swift
 //  Languator
 //
 //  Created by Przemysław Wawrzyniak on 04.05.2016.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LanguatorViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
