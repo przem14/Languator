de429453dc8b831143dbdd255c7036de78d136b7
przem14 <przem.wawrzyniak@gmail.com>
2016-05-04 21:33:34 +0200

    Added add word view in storyboard

---
+++ Languator/WordDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WordDetailViewController.swift
+//  Languator
+//
+//  Created by Przemysław Wawrzyniak on 04.05.2016.
+//  Copyright © 2016 Przemyslaw Wawrzyniak. All rights reserved.
+//
+
+import UIKit
+
+class WordDetailViewController: UITableViewController {
+
+}
