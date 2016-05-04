d8732c8402156f2ac66c773f9537d8a18f846768
przem14 <przem.wawrzyniak@gmail.com>
2016-05-04 20:03:12 +0200

    Initial Commit

---
+++ Languator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Languator
+//
+//  Created by Przemysław Wawrzyniak on 04.05.2016.
+//  Copyright © 2016 Przemyslaw Wawrzyniak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
