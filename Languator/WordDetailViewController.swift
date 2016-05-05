fc7c74946659e121c89a3b263246b8ec0c36c9f6
przem14 <przem.wawrzyniak@gmail.com>
2016-05-05 22:43:34 +0200

    Added data validation to word detail view

--- Languator/WordDetailViewController.swift
+++ Languator/WordDetailViewController.swift
@@ -13,9 +13,11 @@ protocol WordDetailViewControllerDelegate {
     func wordDetailViewController(controller: WordDetailViewController, didAddWordItem item: WordItem)
 }
 
-class WordDetailViewController: UITableViewController {
+class WordDetailViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var foreignWordTextField: UITextField!
     @IBOutlet weak var translationTextField: UITextField!
+
+    @IBOutlet weak var doneButton: UIBarButtonItem!
     
     var delegate: WordDetailViewControllerDelegate?
     
@@ -27,10 +29,33 @@ class WordDetailViewController: UITableViewController {
     }
     
     
+    // MARK: UITextFieldDelegate
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let oldText: NSString = textField.text!
+        let newText: String = oldText.stringByReplacingCharactersInRange(range, withString: string)
+        
+        if textField.restorationIdentifier == "foreignWordTextField" {
+            doneButton.enabled = !newText.isEmpty && !translationTextField.text!.isEmpty
+        } else {
+            doneButton.enabled = !newText.isEmpty && !foreignWordTextField.text!.isEmpty
+        }
+        
+        return true
+    }
+    
+    
     // MARK: Actions
     
     @IBAction func didEndOnExitFromForeignWordTextField() {
         translationTextField.becomeFirstResponder()
+    }
+    
+    @IBAction func didEndOnExitFromTranslationTextField(textField: UITextField) {
+        guard !foreignWordTextField.text!.isEmpty else {
+            return
+        }
+        addWordItem()
     }
     
     @IBAction func cancel() {
